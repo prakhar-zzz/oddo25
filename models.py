ff0aaad321f4a5c4ce9776070e7e6b67171783ce
Krrish7600 <krishlenjhara@gmail.com>
2025-07-12 12:01:34 +0530

    Update models.py
    base 1st iteration

---
+++ models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    points = db.Column(db.Integer, default=100)
+    is_admin = db.Column(db.Boolean, default=False)
