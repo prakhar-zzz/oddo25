7ed28e77b83b3e941bcd0c3489521ecf863a170c
Krrish7600 <krishlenjhara@gmail.com>
2025-07-12 16:51:48 +0530

    Create view_db.py

---
+++ view_db.py
@@ -0,0 +1,12 @@
+from models import db, User
+from flask import Flask
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+with app.app_context():
+    users = User.query.all()
+    for user in users:
+        print(user)
